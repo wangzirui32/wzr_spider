7506942936b806ba79f304fe1640174726ea607f
wangzirui32 <wangzirui32@qq.com>
2021-06-29 18:03:35 +0800

    第一次提交

---
+++ crawler.py
@@ -0,0 +1,39 @@
+import requests
+from fake_useragent import UserAgent as UA
+
+class Crawler():
+    def __init__(self, url_list, parse_func, save_data_func, method="GET"):
+        self.method = method
+        self.url_list = url_list
+        self.parse = parse_func
+        self.save_data = save_data_func
+        self.headers = {
+            "User-Agent": UA().random,
+        }
+    
+    def request(self):
+        try:
+            self.headers = {
+                "User-Agent": UA().random,
+            }
+            self.reponse = requests.request(self.method,
+                                            self.url_list.get_url(),
+                                            headers=self.headers)
+        except Exception:
+            return False
+        else:
+            return True
+
+    def start_parsing(self):
+        try:
+            self.data = self.parse(self.reponse)
+        except Exception:
+            return False
+        else:
+            return True
+
+    def start_crawling(self):
+        while self.url_list.get_urls_size():
+            self.request()
+            self.start_parsing()
+            self.save_data(self.data)
---
+++ urls.py
@@ -0,0 +1,17 @@
+import queue
+
+class UrlList():
+    def __init__(self, *urls):
+        self.url_list = queue.Queue(len(urls))
+
+        for url in urls:
+            self.url_list.put(url)
+
+    def get_url(self):
+        if self.url_list.qsize():
+            return self.url_list.get()
+        else:
+            return None
+
+    def get_urls_size(self):
+        return self.url_list.qsize()
